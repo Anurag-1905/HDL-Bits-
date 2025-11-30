f70984b79903098818a1d434690bd4837b39fc7c
Anurag <anuragchoubey266@gmail.com>
2025-11-30 13:11:08 +0000

    Vector 3

---
+++ Vector3.sv
@@ -0,0 +1,8 @@
+module top_module (
+    input [4:0] a, b, c, d, e, f,
+    output [7:0] w, x, y, z );//
+//Part selection was used to select portions of a vector. The concatenation operator {a,b,c} is 
+//used to create larger vectors by concatenating smaller portions of a vector together. 
+    assign { w,x,y,z } = { a,b,c,d,e,f, 2'b 11 };
+
+endmodule
